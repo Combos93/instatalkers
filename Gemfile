a21d571d4d8c14b7307d85e0968ca0f2a4fa1f29
Combos93 <combos93@mail.ru>
2019-06-12 13:26:02 +0600

    heroku configuration

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'faker'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bootstrap'
+gem 'redis'
 
 gem 'jquery-rails'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -41,10 +41,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+# Mount Action Cable outside main process or domain
+# config.action_cable.mount_path = nil
+ config.action_cable.url = 'wss://instatalkers.herokuapp.com/cable'
+ config.action_cable.allowed_request_origins = ['http://instatalkers.herokuapp.com/cable', 'https://instatalkers.herokuapp.com/cable' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
